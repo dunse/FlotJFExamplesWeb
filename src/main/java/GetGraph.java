59538c73a70ca7fec6701e1c34b9c50e53fa02c0
Pehr Johansson <pehr.l.johansson@gmail.com>
2012-02-21 13:12:29 +1100

    Converted project to servlet 2.5

--- src/main/java/GetGraph.java
+++ src/main/java/GetGraph.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
  * Servlet implementation class GetGraph
  */
-@WebServlet("/GetGraph")
 public class GetGraph extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/GetGraphOptions.java
+++ src/main/java/GetGraphOptions.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /**
  * Servlet implementation class GetGraphOptions
  */
-@WebServlet("/GetGraphOptions")
 public class GetGraphOptions extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
